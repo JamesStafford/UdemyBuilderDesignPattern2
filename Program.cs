335efa785d7370bbb4d1e2a8bf34c1d5298a8232
jstafford <jwstafford1991@gmail.com>
2022-05-21 10:09:27 -0500

    Initial commit from Rider template

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BuilderDesignPattern2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
